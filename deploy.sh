42b399047ce36cc49909150a4a73d8d80c3113d1
John Valentino <jvalentino2@gmail.com>
2023-03-13 15:45:38 -0500

    initial port

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm delete pg-primary --wait || true
+helm install pg-primary \
+	--wait \
+	--set auth.postgresPassword=postgres \
+	--set auth.username=postgres \
+	--set auth.database=examplesys \
+	bitnami/postgresql
+sh -x ./verify.sh
